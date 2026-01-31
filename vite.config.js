a2a6d943e11bf2c165ffa202d93563164171fee4
capungamazon <128035894+r-zaa@users.noreply.github.com>
2026-01-31 17:20:55 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/' // HARUS sama dengan nama repo
+  base: '/portfolio-react/' // HARUS sama dengan nama repo
 })
